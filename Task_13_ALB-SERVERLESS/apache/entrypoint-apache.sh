12b797027ac4e43e0e550efbe96f6e8841c997fe
Max Melnikov <maxik257199@mail.ru>
2025-03-03 15:13:31 +0300

    task13: website(serverless(nginx,apache),api_gateway)

---
+++ Task_13_ALB-SERVERLESS/apache/entrypoint-apache.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "---------Start entrypoint-apache.sh"
+
+if [ -z "$PORT" ]; then
+  echo "-----------ERROR: PORT environment variable is not set!"
+  exit 1
+fi
+
+echo "---------Using PORT: $PORT"
+
+echo "ServerName 0.0.0.0" >> /etc/apache2/apache2.conf
+
+# Убеждаемся, что Apache слушает на всех интерфейсах
+echo "Listen 0.0.0.0:$PORT" > /etc/apache2/ports.conf
+sed -i "s/<VirtualHost .*>/<VirtualHost 0.0.0.0:$PORT>/" /etc/apache2/sites-enabled/000-default.conf
+
+# Запускаем Apache в foreground-режиме (иначе контейнер завершится)
+exec apache2-foreground
+
+tail -f /var/log/apache2/access.log
---
+++ Task_13_ALB-SERVERLESS/nginx/entrypoint-serverless.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "---------Start entrypoint-serverless.sh"
+
+
+# Проверяем, что переменные окружения заданы
+if [ -z "$PORT" ]; then
+  echo "----------ERROR: PORT environment variable is not set!"
+  exit 1
+fi
+if [ -z "$DOMAIN" ]; then
+  echo "----------ERROR: DOMAIN environment variable is not set!"
+  exit 1
+fi
+
+echo "----------Using PORT: $PORT"
+echo "----------Using DOMAIN: $DOMAIN"
+
+
+# Generate file *.conf with vars from .env
+for file in /etc/nginx/sites-enabled/*.template; do
+    envsubst < "$file" > "${file%.template}"
+    rm "$file"
+done
+echo "---------Conf files generated from .template"
+
+
+nginx &
+
+
+echo "---------Nginx launched"
+# cat /etc/nginx/sites-enabled/*
+
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log
